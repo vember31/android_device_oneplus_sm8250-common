8cda99fe4504866decf45d8216c4bb11360279d4
vember31 <prestigioustennis@gmail.com>
2025-02-16 10:41:01 -0600

    remove oplus cam

--- common.mk
+++ common.mk
@@ -399,7 +399,7 @@ PRODUCT_PACKAGES += \
     firmware_WCNSS_qcom_cfg.ini_symlink
 
 # Oneplus Camera
-$(call inherit-product, vendor/oneplus/camera/camera-vendor.mk)
+# $(call inherit-product, vendor/oneplus/camera/camera-vendor.mk)
 
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/sm8250-common/sm8250-common-vendor.mk)
